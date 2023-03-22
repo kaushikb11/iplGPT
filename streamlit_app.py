cb58b21edc4de94c4de528a179d933a4b59de46c
Kaushik Bokka <kaushikbokka@gmail.com>
2023-03-22 15:02:47 +0530

    Update streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,28 +21,49 @@ headers = {
 
 query = st.text_input("Ask any question about IPL", key="query")
 
-if len(st.session_state.query) > 0:
-    params = {
-        "search_query": st.session_state.query,
-    }
-    response = requests.post(
-        "https://kaushikb11--ipl-gpt-fastapi-app.modal.run/search",
-        params=params,
-        headers=headers,
-    )
-    # response, data = send_query_to_agent(agent, st.session_state.query)
-    response = response.json()
-    result = response["result"]
-    data = response["data"]
 
-    st.markdown(result)
-    data = ast.literal_eval(data)
-    if len(data) > 1:
-        values = [value[1] for value in data]
-        indexes = [index[0] for index in data]
-        data = pd.DataFrame(values, index=indexes, columns=["Count"])
-        # data.reset_index(drop=True, inplace=True)
+def button_click(query):
+    st.session_state.query = query
+
+
+QUESTIONS = [
+    "Top 5 players with the most Player of the Match awards?",
+    "Which team won the maximum number of matches in IPL 2020?",
+    "What's the number of runs scored by AB de Villiers season wise?",
+]
+
+for question in QUESTIONS:
+    st.button(
+        question,
+        on_click=button_click,
+        args=(question,),
+        use_container_width=True,
+        type="primary",
+    )
+
+if len(st.session_state.query) > 0:
+    with st.spinner(text="In progress..."):
+        params = {
+            "search_query": st.session_state.query,
+        }
+        response = requests.post(
+            "https://kaushikb11--ipl-gpt-fastapi-app.modal.run/search",
+            params=params,
+            headers=headers,
+        )
+        # response, data = send_query_to_agent(agent, st.session_state.query)
+        response = response.json()
+        result = response["result"]
+        data = response["data"]
+
+        st.markdown(result)
+        data = ast.literal_eval(data)
         if len(data) > 1:
-            df, chart = st.columns(2)
-            df.dataframe(data, use_container_width=True)
-            chart.bar_chart(data)
+            values = [value[1] for value in data]
+            indexes = [index[0] for index in data]
+            data = pd.DataFrame(values, index=indexes, columns=["Count"])
+            # data.reset_index(drop=True, inplace=True)
+            if len(data) > 1:
+                df, chart = st.columns(2)
+                df.dataframe(data, use_container_width=True)
+                chart.bar_chart(data)
