375fbd37495d88c7eb405e565a91cca7bef0e608
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2023-05-29 05:39:33 +0530

    Update default.py

--- ipl_gpt/agents/sql/default.py
+++ ipl_gpt/agents/sql/default.py
@@ -1,13 +1,8 @@
-import os
-
 from langchain.agents import create_sql_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.llms.openai import OpenAI
 from langchain.sql_database import SQLDatabase
-
-# TODO: Remove it later
-os.environ["OPENAI_API_KEY"] = "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"
 
 
 def default_sql_agent(sqlite_url: str) -> AgentExecutor:
