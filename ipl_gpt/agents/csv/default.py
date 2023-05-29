2e3fa16a327cbecdfedace6d3fdff830e105ed4c
Kaushik B <45285388+kaushikb11@users.noreply.github.com>
2023-05-29 05:39:53 +0530

    Update default.py

--- ipl_gpt/agents/csv/default.py
+++ ipl_gpt/agents/csv/default.py
@@ -1,11 +1,6 @@
-import os
-
 from langchain.agents import create_csv_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.llms.openai import OpenAI
-
-# TODO: Remove it later
-os.environ["OPENAI_API_KEY"] = "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"
 
 
 def default_csv_agent(csv_file: str) -> AgentExecutor:
